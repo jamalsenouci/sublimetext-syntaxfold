8d522f662eaa11ee255e3a77614f97be5f875b79
jamal <jamalsenouci@gmail.com>
2014-09-11 16:10:21 +0100

    generalise code to read in settings file and added keymap

--- SyntaxFold.py
+++ SyntaxFold.py
@@ -1,46 +1,50 @@
 import sublime, sublime_plugin
 
+s = sublime.load_settings('syntax_fold.sublime-settings')
+startMarker = s.get("startMarker")
+endMarker = s.get("endMarker")
+
+class FoldCommand(sublime_plugin.TextCommand):
+	def run(self, edit, all=None):
+
+
 class FoldAllCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
-		
-		sectionpos = self.view.find_all("'#Section")
-		for x in range(len(sectionpos)):
-			sectionpos[x]=self.view.line(sectionpos[x].end()).end()
-		endpos = self.view.find_all("'#End")
-		for x in range(len(sectionpos)):
+	def run(self, edit):	
+		startpos = self.view.find_all(startMarker)
+		for x in range(len(startpos)):
+			startpos[x]=self.view.line(startpos[x].end()).end()
+		endpos = self.view.find_all(endMarker)
+		for x in range(len(startpos)):
 			endpos[x]=self.view.line(endpos[x].begin()).begin() - 1
 		
 		for x in range( len(endpos)):
-			content = sublime.Region(sectionpos[x], endpos[x])
+			content = sublime.Region(startpos[x], endpos[x])
 			new_content=[content]
 			if content.size() > 0:
 				if self.view.fold(content) == False:
 					new_content = self.view.fold(content)
 			self.selection = new_content
 
-
 class UnfoldAllCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
+		startpos = self.view.find_all(startMarker)
+		for x in range(len(startpos)):
+			startpos[x]=self.view.line(startpos[x].end()).end()
 		
-		sectionpos = self.view.find_all("'#Section")
-		for x in range(len(sectionpos)):
-			sectionpos[x]=self.view.line(sectionpos[x].end()).end()
-		
-		endpos = self.view.find_all("'#End")
-		for x in range(len(sectionpos)):
+		endpos = self.view.find_all(endMarker)
+		for x in range(len(startpos)):
 			endpos[x]=endpos[x].begin()
 
 		for x in range( len(endpos)):
-			content = sublime.Region(sectionpos[x], endpos[x])
+			content = sublime.Region(startpos[x], endpos[x])
 			new_content=[content]
 			if content.size() > 0:
 				new_content = self.view.unfold(content)
 			self.selection = new_content
 
-
 class FoldCurrentCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		sections = self.view.find_all("'#Section")
+		sections = self.view.find_all(startMarker)
 		sectionStart=[]
 		sectionEnd=[]
 		sectionLineEnd=[]
@@ -51,7 +55,7 @@ class FoldCurrentCommand(sublime_plugin.TextCommand):
 
 		endStart=[]
 		endEnd=[]
-		ends = self.view.find_all("'#End")
+		ends = self.view.find_all(endMarker)
 		for x in range(len(ends)):
 			endStart.append(ends[x].begin()-1)
 			endEnd.append(ends[x].end())
@@ -67,7 +71,7 @@ class FoldCurrentCommand(sublime_plugin.TextCommand):
 
 class UnfoldCurrentCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		sections = self.view.find_all("'#Section")
+		sections = self.view.find_all(startMarker)
 		sectionStart=[]
 		sectionEnd=[]
 		sectionLineEnd=[]
@@ -78,7 +82,7 @@ class UnfoldCurrentCommand(sublime_plugin.TextCommand):
 
 		endStart=[]
 		endEnd=[]
-		ends = self.view.find_all("'#End")
+		ends = self.view.find_all(endMarker)
 		for x in range(len(ends)):
 			endStart.append(ends[x].begin()-1)
 			endEnd.append(ends[x].end())
